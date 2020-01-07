f58e8a3823c3d7a4ceffcba06e8ad22cdeabe2cf
Dusan Klinec <dusan.klinec@gmail.com>
2020-01-07 11:02:40 +0100

    all squashed signed
    add .gitignore
    
    import the code
    
    add matrix
    
    (one week after)
    
    import the code (but squashed)
    
    fixed unit tests
    
    fix travis
    
    actually adding travis to the commit
    
    fixing travis again
    
    travis fix - testing multiple versions

---
+++ ossdev/main.py
@@ -0,0 +1,20 @@
+from ossdev import Vector, Matrix
+
+
+def main():
+    a = Vector([0, 1, 2, 3, 4])
+    b = Vector([4, 3, 2, 1, 0])
+    print('Vector a: %s' % a)
+    print('Vector b: %s' % b)
+    print('Vector a+2: %s' % (a+2))
+    print('Vector a+b: %s' % (a+b))
+
+    m = Matrix((3, 3))
+    print(m)
+
+    m1 = Matrix.ident(4)
+    print(m1)
+    print(m1 + m1)
+
+if __name__ == '__main__':
+    main()
