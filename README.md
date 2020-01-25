b801d46b80af153f00e43b1d86b8f510f6bba5ad
Jan Dvořák <jaydee_@outlook.com>
2020-01-25 18:52:00 +0100

    Add squashed commits

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Build Status](https://travis-ci.com/JayDee81/open-source-development-course-hw02-1.svg?branch=master)](https://travis-ci.com/JayDee81/open-source-development-course-hw02-1)
 # Simple Vector implementation in python 
 
 Very simple vector implementation supporting basic operations.
@@ -13,6 +14,13 @@ print(a)
 Operations:
 - Addition with a scalar `a+1`
 - Vector addition: `a + b`
+- Set vector item `v.__setitem__(1, 2)`
+- Compare vectors `v1.__cmp__(v2)`
+- Reverse vector `v.__reversed__()`
+- Subtract vectors `v1.__sub__(v2)`
+- Multiply vector by scalar `v.__mul__(5)`
+- Bit-wise xor vector with scalar `v.__xor__(4)`
+- Vector length `v.length()`
 
 ## Installation
 
--- ossdev/tests/vct_test.py
+++ ossdev/tests/vct_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # Author: ossdev
-
+import math
 import unittest
 from ossdev import Vector
 
@@ -19,6 +19,51 @@ class VectorTest(unittest.TestCase):
 
         self.assertEqual(c.get(), [3, 3, 3, 3])
 
+    def test_setitem(self):
+        a = Vector([0, 1, 2, 3])
+        a.__setitem__(1, 5)
+
+        self. assertEqual(a[1], 5)
+
+    def test_cmp(self):
+        a = Vector([0, 1, 2, 3])
+        b = Vector([3, 2, 1, 0])
+        c = a.__cmp__(b)
+
+        self.assertEqual(c, 0)
+
+    def test_reversed(self):
+        a = Vector([0, 1, 2, 3])
+        a = a.__reversed__()
+
+        self.assertEqual(a, [3, 2, 1, 0])
+
+    def test_sub(self):
+        a = Vector([0, 1, 2, 3])
+        b = a.__sub__(a)
+
+        self.assertEqual(b, [0, 0, 0, 0])
+
+    def test_mul(self):
+        a = Vector([0, 1, 2, 3])
+        b = a.__mul__(2)
+
+        self.assertEqual(b, [0, 2, 4, 6])
+
+    def test_xor(self):
+        a = Vector([0, 1, 2, 3])
+        b = a.__xor__(2)
+        c = [0 ^ 2, 1 ^ 2, 2 ^ 2, 3 ^ 2]
+
+        self.assertEqual(b, c)
+
+    def test_length(self):
+        a = Vector([0, 1, 2, 3])
+        b = a.length()
+        c = math.sqrt(14)
+
+        self.assertEqual(b, c)
+        
 
 if __name__ == "__main__":
     unittest.main()  # pragma: no cover
