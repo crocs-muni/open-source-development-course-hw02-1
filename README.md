6c5b5e348197d78103c6955a4a22368a4e33295f
valcikeric <eric.valcik@gmail.com>
2020-06-11 13:51:59 +0200

    Add matrix and implement all TODOs

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+[![Build Status](https://travis-ci.org/valcikeric/open-source-development-course-hw02-1.svg?branch=pr%2Fstep1)](https://travis-ci.org/valcikeric/open-source-development-course-hw02-1)
 # Simple Vector implementation in python 
 
 Very simple vector implementation supporting basic operations.
@@ -5,14 +6,42 @@ Very simple vector implementation supporting basic operations.
 ## Usage
 
 ```python
-from ossdev import Vector
+from ossdev import Vector, Matrix
 a = Vector([0, 1, 2, 3])
 print(a)
+
+m = Matrix.ident(4)
+print(m)
+print(m + m)
 ```
 
 Operations:
 - Addition with a scalar `a+1`
 - Vector addition: `a + b`
+- Multiplication:
+  - scalar * vector
+  - row-vector * col-vector
+  - col-vector * row-vector
+- Vector substraction: `a - b`
+- Invert vector: `-a`
+- Reverse vector: `reversed(a)`
+- Vector XOR with scalar: `a ^ 3`
+- Vector XOR with another vector: `a ^ b`
+- Vector AND with scalar: `a & 4`
+- Vector AND with another vector: `a & b`
+- Vector length: `a.length()`
+- Vector length comparison: 
+```python
+a == b
+a != b
+a < b
+a <= b
+a > b
+b >= a
+```
+
+Matrix operations:
+- Addition
 
 ## Installation
 
--- ossdev/tests/vct_test.py
+++ ossdev/tests/vct_test.py
@@ -3,7 +3,7 @@
 # Author: ossdev
 
 import unittest
-from ossdev import Vector
+from ossdev import Vector, Matrix
 
 
 class VectorTest(unittest.TestCase):
@@ -18,6 +18,107 @@ class VectorTest(unittest.TestCase):
         c = a + b
 
         self.assertEqual(c.get(), [3, 3, 3, 3])
+ 
+    def test_dot(self):
+        a = Vector([1, 2])
+        b = Vector([2, -1])
+        self.assertEqual(a.dot(b), 0)
+    
+    def test_setitem(self):
+        a = Vector([0, 0])
+        a[1] = 4
+
+        self.assertEqual(a.d, [0, 4])
+    
+    def test_eq(self):
+        a = Vector([1, 2, 3])
+        b = Vector([1, -2, 3])
+
+        self.assertEqual(a == b, True)
+
+    def test_cmp(self):
+        a = Vector([1, 1])
+        b = Vector([1, 3])
+
+        self.assertEqual(a > b, False)
+        self.assertEqual(b > a, True)
+    
+    def test_eq(self):
+        a = Vector([1, 2, 4])
+        b = Vector([1, 2, 3])
+
+        self.assertFalse(a == b)
+
+    def test_reversed(self):
+        a = Vector([1, 2, 3])
+        rev = reversed(a)
+
+        self.assertEqual(rev, Vector([3, 2, 1]))
+    
+    def test_sub(self):
+        a = Vector([3, 3, 2])
+        b = Vector([1, 1, 1])
+        c = a - b
+
+        self.assertEqual(c, Vector([2, 2, 1]))
+    
+    def test_mul_with_scalar(self):
+        a = Vector([1, 2, 3]) * 2
+
+        self.assertEqual(a, Vector([2, 4, 6]))
+
+    def test_mul_with_vector(self):
+        a = Vector([1, 2, 1]) * Vector([1, 2, 3])
+
+        self.assertEqual(a, Vector([1, 4, 3]))
+
+        b = Vector([1, 2, 3])
+        b.row = True
+        dot_product = b * Vector([2, 3, 4])
+
+        self.assertEqual(dot_product, 20)
+    
+    def test_xor(self):
+        a = Vector([7, 3])
+        ret = a ^ 3
+
+        self.assertEqual(ret, Vector([4, 0]))
+    
+    def test_and_scalar(self):
+        a = Vector([1, 2, 3])
+
+        self.assertEqual(a & 1, Vector([1, 0, 1]))
+
+    def test_and_vector(self):
+        a = Vector([1, 2, 3])
+        b = Vector([2, 3, 4])
+
+        self.assertEqual(a & b, Vector([0, 2, 0]))
+
+    def test_lenght(self):
+        self.assertAlmostEqual(Vector([2, 4]).length(), 4.47213595499, 3)
+        self.assertAlmostEqual(Vector([2, 4, 5]).length(), 6.708203932499369, 3)
+        return
+    
+    def test_transpose(self):
+        a = Vector([1, 2, 3])
+        b = a.transpose()
+
+        self.assertTrue(b.row)
+    
+    def test_matrix_add(self):
+        a = Matrix.square(2)
+        b = Matrix.ident(2)
+
+        a[0][0] = 4
+        b[0][1] = 3
+
+        ab = a + b
+
+        self.assertEqual(ab[0][0], 5)
+        self.assertEqual(ab[0][1], 3)
+        self.assertEqual(ab[1][0], 0)
+        self.assertEqual(ab[1][1], 1)
 
 
 if __name__ == "__main__":
